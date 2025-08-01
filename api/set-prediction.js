e1e54a279cf31330293f6cb59d31e12b68dbf86f
magicoppk27 <134200712+magicoppk27@users.noreply.github.com>
2025-08-02 00:42:09 +0100

    Update set-prediction.js

--- api/set-prediction.js
+++ api/set-prediction.js
@@ -1,15 +1,6 @@
 import { kv } from '@vercel/kv';
 
 export default async function handler(req, res) {
-  // Set CORS headers to allow requests from any origin
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  // Handle preflight requests for CORS
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
