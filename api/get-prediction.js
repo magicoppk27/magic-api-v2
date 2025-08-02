b1607e37cc348355b912224c1dc0efc8b3f49023
magicoppk27 <134200712+magicoppk27@users.noreply.github.com>
2025-08-02 01:56:01 +0100

    Update get-prediction.js

--- api/get-prediction.js
+++ api/get-prediction.js
@@ -1,6 +1,12 @@
 import { kv } from '@vercel/kv';
 
 export default async function handler(req, res) {
+  // This block ensures GET requests also have the correct permissions.
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allows any origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   try {
     const prediction = await kv.get('prediction');
     const currentPrediction = prediction || "[No prediction set]";
